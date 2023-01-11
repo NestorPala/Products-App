2b374ef930e2e2aca05190ca4377c30169bf759c
NestorPala <npalavecino@fi.uba.ar>
2023-01-11 14:18:21 -0300

    added build_onrender.sh script

---
+++ build_onrender.sh
@@ -0,0 +1,3 @@
+# Build script to deploy on render.com
+
+rm -rf node_modules package-lock.json && npm i && tsc --project tsconfig.json && tsc-alias -p tsconfig.json && cd app && npm i && npx -p @angular/cli@14 ng build && cd ..
